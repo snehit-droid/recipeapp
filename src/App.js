4fef2486382f1b5ff45be61c5898a63155e754e0
Snehit <snehitchats@gmail.com>
2020-12-31 17:46:02 +0530

    final

--- src/App.js
+++ src/App.js
@@ -11,14 +11,12 @@ const App = () => {
   const [query, setQuery] = useState('chicken');
     
   useEffect( async () => {
-    getRecipes()
+    const getRecipes = async () => {
+      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      const data = await response.json();
+      setRecipes(data.hits);
+    }
   }, [query]);
-
-  const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-    const data = await response.json();
-    setRecipes(data.hits);
-  }
 
   const updateSearch = (e) => {
     setSearch(e.target.value);    // 'e' here refers to event. 
